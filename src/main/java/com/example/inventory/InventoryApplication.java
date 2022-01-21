7600ae1f9c83d6e8475778901053f5da4cbdb325
aadesh.agarwal8888 <aadesh.agarwal8888@gmail.com>
2022-01-20 23:38:44 -0500

    CORS updated

--- src/main/java/com/example/inventory/InventoryApplication.java
+++ src/main/java/com/example/inventory/InventoryApplication.java
@@ -18,7 +18,7 @@ public class InventoryApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000")
+                registry.addMapping("/**").allowedOrigins("*")
                 .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
             }
         };
